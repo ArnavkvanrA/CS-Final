669d98bd21e40f9d2c105f3e5a71d62f9ba2624e
Pranay C <121254632+matrixvirtual@users.noreply.github.com>
2025-05-20 21:06:20 +0000

    2:05 pm on 5/20

--- App.java
+++ App.java
@@ -1,12 +1,16 @@
 import javax.swing.*;
 import java.awt.*;
-public class App{
-    public static void main(String[] args){
-        JFrame frame = new JFrame("My First GUI");
+public class App {
+    public static void main(String[] args) {
+        //Create the frame.
+        JFrame frame = new JFrame("Test");
+
+        JPanel canvas = new JPanel();
+        canvas.setPreferredSize( new Dimension( 800, 600 ) );
+
+        frame.add(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 200);
-        JLabel label = new JLabel("Hello, Swing!", SwingConstants.CENTER);
-        frame.getContentPane().add(label);
-        frame.setVisible(true); 
+        frame.pack();
+        frame.setVisible(true);
     }
 }
