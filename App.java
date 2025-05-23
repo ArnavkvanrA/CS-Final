db8c65a74aed543b9e45da50d84794548b8a0d97
ArnavkvanrA <ak6372@pleasantonusd.net>
2025-05-23 15:39:03 +0000

    Implementing a timer system

--- App.java
+++ App.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 public class App {
     public static void main(String[] args) {
         //Create the frame.
@@ -8,9 +10,22 @@ public class App {
         JPanel canvas = new JPanel();
         canvas.setPreferredSize( new Dimension(920, 1080) );
 
+        JLabel label = new JLabel("Starting...");
+        label.setFont(new Font("Arial", Font.BOLD, 32));
+        canvas.add(label);
+        
         frame.add(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+
+        Timer timer = new Timer(1000, new ActionListener() {
+            int counter = 0;
+            public void actionPerformed(ActionEvent e) {
+                counter++;
+                label.setText("Seconds passed: " + counter);
+            }
+        });
+        timer.start();
     }
 }
